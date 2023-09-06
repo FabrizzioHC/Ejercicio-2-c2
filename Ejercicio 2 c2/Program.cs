7965af96ad22e2b1185020bf54acaa717e2e3768
FABRIZZIO <FABRIZZIO@DESKTOP-8PLVDLM>
2023-09-05 21:57:05 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 c2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_c2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String tipofruta;
+            int distanciaKilometros;
+            int costocaja;
+            Console.WriteLine("ingresa el nombre de la fruta que va a llevar (Naranja, Fresa o Mango): ");
+            tipofruta = Console.ReadLine();
+            Console.WriteLine("ingresa la distancia en kilometros: ");
+            distanciaKilometros = int.Parse(Console.ReadLine());
+            switch (tipofruta)
+            {
+                case "Naranja":
+                    if (distanciaKilometros < 30)
+                    {
+                        costocaja = 2;
+                    }
+                    else
+                    {
+                        costocaja = 4;
+                    }
+                    break;
+                case "Fresa":
+                    if (distanciaKilometros < 30)
+                    {
+                        costocaja = 6;
+                    }
+                    else
+                    {
+                        costocaja = 9;
+                    }
+                    break;
+                case "Mango":
+                    if (distanciaKilometros < 30)
+                    {
+                        costocaja = 3;
+                    }
+                    else
+                    {
+                        costocaja = 5;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("la fruta que elijio no esta en nuestra lista.");
+                    return;
+            }
+
+            Console.WriteLine($"el costo por caja de frutas es: S/{costocaja}");
+            Console.ReadKey();
+        }
+    }
+}
